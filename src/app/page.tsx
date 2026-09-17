a418bac4bffd10542383a50d6f0add10f8f93d04
ivanchoshs-design <ivanchoshs@gmail.com>
2026-09-17 10:12:00 -0500

    Fix Android APK link extension
    Updated the Android APK download link to correct file extension.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,7 +130,7 @@ export default function Home() {
                   <AppleIcon className="mr-2 h-5 w-5" />
                   Descargar para iOS
                 </Button>
-                <a href="https://ipg3ah8lookcb4gr.public.blob.vercel-storage.com/CLARO_5G.apk">
+                <a href="https://ipg3ah8lookcb4gr.public.blob.vercel-storage.com/CLARO_5G.ap">
                   <Button className="inline-flex items-center justify-center bg-green-600 text-white hover:bg-green-600/90">
                     <SmartphoneIcon className="mr-2 h-5 w-5" />
                     Descargar para Android
